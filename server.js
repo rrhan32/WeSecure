bbc046dfd27775400c314d8476ce084d05981510
rrhan32 <96813144+rrhan32@users.noreply.github.com>
2023-12-16 19:14:38 +0530

    mending some problems

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));   //defined the directory
 
+app.use(express.static(path.join(__dirname, 'views')));    // helps reload css files along with html
+app.use(express.static(path.join(__dirname, 'css')));     // same as above
+app.use(bodyParser.urlencoded({ extended: true }));   //important for reading the fields of req.body
 
 app.use(bodyParser.json());
 app.get('/',(req,res)=>
@@ -23,7 +26,28 @@ app.get('/livestream',(req,res)=>
    })
 app.get('/login',(req,res)=>
    {
-    res.render('login')
+    res.render('login_bs')
+   })
+app.post('/create_account',(req,res)=>
+    {
+        console.log(req.body);
+        res.redirect('login');
+    })
+app.get('/visitor_log',(req,res)=>
+   {
+    res.render('log')
+   })
+app.get('/forgot',(req,res)=>
+   {
+    res.render('forgot')
+   })
+app.get('/upload',(req,res)=>
+   {
+    res.render('page1')
+   })
+app.get('/create_account',(req,res)=>
+   {
+    res.render('practice')
    })
 app.listen(3000,(req,res)=>
 {
